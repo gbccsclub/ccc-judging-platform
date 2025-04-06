3e574ffe235a499202c5e801a67db3ffb7b05efa
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-06 10:52:15 -0400

    style: bold button font

--- src/services/flowbite.ts
+++ src/services/flowbite.ts
@@ -3,10 +3,10 @@ import { createTheme } from "flowbite-react";
 export const customTheme = createTheme({
     button: {
         color: {
-            primary: "bg-blue-500 text-white hover:bg-blue-600 hover:text-white transition duration-200 ease-in-out",
-            secondary: "bg-blue-200 text-blue-600 hover:bg-blue-500 hover:text-white transition duration-200 ease-in-out",
-            success: "bg-green-200 text-green-600 hover:bg-green-500 hover:text-white transition duration-200 ease-in-out",
-            failure: "bg-red-200 text-red-600 hover:bg-red-500 hover:text-white transition duration-200 ease-in-out",
+            primary: "bg-blue-500 text-white hover:bg-blue-600 hover:text-white transition duration-200 ease-in-out font-bold",
+            secondary: "bg-blue-200 text-blue-600 hover:bg-blue-500 hover:text-white transition duration-200 ease-in-out font-bold",
+            success: "bg-green-200 text-green-600 hover:bg-green-500 hover:text-white transition duration-200 ease-in-out font-bold",
+            failure: "bg-red-200 text-red-600 hover:bg-red-500 hover:text-white transition duration-200 ease-in-out font-bold",
         },
     },
     popover: {
