ad8765cbddfd06a20a312c033106cfe5fb6d967c
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-06 11:31:46 -0400

    refac: update title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -10,7 +10,7 @@ export default function Title ({
     return <>
         <div className={'flex flex-col justify-start mb-4 ' + (isLoggedIn ? 'fixed top-4 left-4' : '')}>
             <h1 className={(isLoggedIn ? "text-lg" : "text-3xl") + " text-left font-serif text-gray-700"}>
-                Creative Coding Judging Platform
+                Creative Coding Challenge
             </h1>
             <p className={(isLoggedIn ? "text-sm" : "text-lg") + ' text-gray-500'}>
                 by GBC CS Club
