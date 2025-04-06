e49c4b7e99351acf4990dfc1d8785c31819cbde1
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-06 11:21:37 -0400

    feat: close button for message dialog

--- src/components/MessageDialog.tsx
+++ src/components/MessageDialog.tsx
@@ -1,19 +1,26 @@
-import { Alert } from "flowbite-react";
+import { Alert, Button } from "flowbite-react";
 import { useMessage } from "../context/MessageContext";
 
 export default function MessageDialog() {
-    const { message } = useMessage();
-    
+    const { message, setMessage } = useMessage();
+
     if (!message) return <></>;
     return <>
         <Alert
-            className="fixed bottom-4 right-4 z-50"
+            className="fixed bottom-4 right-4 z-50 flex flex-row"
             color={message.type === 'success' ? 'success' : 'failure'}>
             {message.type === 'success'
                 ? <i className="fa-solid fa-circle-check"></i>
                 : <i className="fa-solid fa-circle-exclamation"></i>}
             <span> </span>
             {message.text}
+
+            <button
+                className='p-2 hover:-translate-y-0.5 active:translate-y-0 hover:text-blue-500 transition duration-200 ease-in-out cursor-pointer'
+                onClick={() => setMessage(null)}
+            >
+                <i className="fa-solid fa-xmark"></i>
+            </button>
         </Alert>
     </>
 }
