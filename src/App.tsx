9aecad601addbb4690343e15cfa9ae6af3c9e1c1
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-06 15:41:49 -0400

    feat: animate message box

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Title from './components/Title'
 import SignedInView from './views/SignedInView'
 import SignedOutView from './views/SignedOutView'
 import { useSession } from './context/SessionContext'
+import { useEffect } from 'react';
+import { useMessage } from './context/MessageContext';
 
 function App() {
     const { session, isLoading } = useSession();
@@ -9,6 +11,14 @@ function App() {
     if (isLoading) {
         return <div>Loading...</div>;
     }
+    const { setMessage } = useMessage();
+
+    useEffect(() => {
+        setMessage({
+            type: 'info',
+            text: 'Welcome to the Judging App!'
+        });
+    }, []);
 
     return <>
         <div className='flex flex-col justify-center items-center h-[90vh]'>
--- src/components/MessageDialog.tsx
+++ src/components/MessageDialog.tsx
@@ -1,10 +1,9 @@
 import { Alert } from "flowbite-react";
 import { useMessage } from "../context/MessageContext";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function MessageDialog() {
     const { message, setMessage } = useMessage();
-
-    if (!message) return <></>;
 
     const colorMap = {
         success: 'success',
@@ -20,19 +19,30 @@ export default function MessageDialog() {
         warning: 'fa-circle-exclamation'
     };
 
-    return <>
-        <Alert
-            className="fixed bottom-4 right-4 z-50 flex flex-row"
-            color={colorMap[message.type]}>
-            <i className={'fa-solid ' + iconMap[message.type]}></i>
-            <span> </span>
-            {message.text}
-            <button
-                className='p-2 hover:-translate-y-0.5 active:translate-y-0 hover:text-blue-500 transition duration-200 ease-in-out cursor-pointer'
-                onClick={() => setMessage(null)}
+    return <AnimatePresence>
+        {message &&
+            <motion.div
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.4, ease: 'easeInOut' }}
+                className="fixed top-4 right-4 z-50"
             >
-                <i className="fa-solid fa-xmark"></i>
-            </button>
-        </Alert>
-    </>
+                <Alert
+                    className="flex flex-row"
+                    color={colorMap[message.type]}
+                >
+                    <i className={'fa-solid ' + iconMap[message.type]}></i>
+                    <span> </span>
+                    {message.text}
+                    <button
+                        className='p-2 hover:-translate-y-0.5 active:translate-y-0 hover:text-blue-500 transition duration-200 ease-in-out cursor-pointer'
+                        onClick={() => setMessage(null)}
+                    >
+                        <i className="fa-solid fa-xmark"></i>
+                    </button>
+                </Alert>
+            </motion.div>
+        }
+    </AnimatePresence>;
 }
