b9a68bd0ac66299231744976df179a8531ab2f3e
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-06 20:14:14 -0400

    style: card border darker

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -37,7 +37,7 @@ export default function PostCard({ post }: PostCardProps) {
     return (
         <div
             ref={cardRef}
-            className="rounded-lg overflow-hidden border border-gray-100"
+            className="rounded-lg overflow-hidden border border-gray-200"
         >
             <div className="relative w-100 h-110 bg-gray-100 overflow-hidden">
                 {isVisible &&
